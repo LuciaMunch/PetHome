e85980bf94ff704dca169463e5fbf6797dd7aefa
LuciaMunch <lucia.munch@hotmail.com>
2026-07-28 22:54:32 -0300

    Proyecto base Spring Boot

---
+++ backend/src/test/java/com/pethome/PethomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pethome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PethomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
